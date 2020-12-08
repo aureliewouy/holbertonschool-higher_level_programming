ca21c51675674acf24ec245f38b25d997c973f56
aureliewouy <aureliecedia@yahoo.com>
2020-12-08 15:07:49 +0000

    fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizzbuzz", end=' ')
         elif i % 3 == 0:
             print("Fizz", end=' ')
-          elif i % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz", end=' ')
         else:
             print("{:d}".format(i), end=' ')
