29572090f79e12339c8b81fb053513a6f9370dec
aureliewouy <aureliecedia@yahoo.com>
2020-12-10 13:26:39 +0000

    fix 0args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    if len(argv) == 2:
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
         print("{:d} argument".format(len(argv) - 1))
         print("1: {}".format(argv[1]))
     else:
