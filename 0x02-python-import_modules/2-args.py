93496829c63a38d829551d8850e61d31ddc56049
aureliewouy <aureliecedia@yahoo.com>
2020-12-10 13:24:18 +0000

    first commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 2:
+        print("{:d} argument".format(len(argv) - 1))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{:d} arguments".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
