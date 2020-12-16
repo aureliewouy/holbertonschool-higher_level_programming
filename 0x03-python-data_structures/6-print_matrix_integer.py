86ce2dd62636ed033ed9da5bbc104f0e74b45888
aureliewouy <aureliecedia@yahoo.com>
2020-12-16 15:39:53 +0000

    fix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end="")
-            if j % 3 != 0:
-                print(end=" ")
-        print()
+        print(" ".join("{:d}".format(j) for j in i))
