513d8b9fbd1e107e85bce98791692ec977d8c049
aureliewouy <aureliecedia@yahoo.com>
2020-12-17 19:16:55 +0000

    fix check

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def square_by_two(num):
-    num = num ** 2
-    return num
-
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
-        new_matrix.append(list(map(square_by_two, i)))
+        new_matrix.append(list(map(lambda x: x **2, i)))
     return new_matrix
