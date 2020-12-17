ef0e00d4f66f18c706b4414f5c0183769ed0d70f
aureliewouy <aureliecedia@yahoo.com>
2020-12-17 19:13:47 +0000

    fix tabulation

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,5 +6,5 @@ def square_by_two(num):
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
-            new_matrix.append(list(map(square_by_two, i)))
+        new_matrix.append(list(map(square_by_two, i)))
     return new_matrix
