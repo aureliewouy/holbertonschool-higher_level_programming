1785ed133af75711f95f636e84b7aae61bddfc84
aureliewouy <aureliecedia@yahoo.com>
2021-01-11 17:56:51 +0000

    compliance PEP8

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
 Task 2 - Area and Perimeter
-
 """
+
 
 class Rectangle:
     """Class that defines a rectangle"""
     def __init__(self, width=0, height=0):
         """ Initialise Rectangle """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
