da0e8632fc9648edc471696a588e4d1f6e23eb80
aureliewouy <aureliecedia@yahoo.com>
2021-01-20 18:47:08 +0000

    change 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,11 +2,11 @@
 """
 Class to JSON
 """
-import json
+
 
 def class_to_json(obj):
     """
     Returns the dictionary description for JSON serialization of an object
     """
-    s = json.dumps(obj.__dict__)
-    return json.loads(s)
+
+    return obj.__dict__
