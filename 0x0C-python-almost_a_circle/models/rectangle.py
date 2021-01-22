1e527130633984708bd7c862c0d051c57a6d711e
aureliewouy <aureliecedia@yahoo.com>
2021-01-22 13:42:15 +0000

    update the class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 """
 First Rectangle
 """
-
+from sys import argv
 from models.base import Base
 
 
@@ -27,7 +27,7 @@ class Rectangle(Base):
         """ set the width """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -41,7 +41,7 @@ class Rectangle(Base):
         """ set the height """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -71,8 +71,54 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
-            self.__y = value
+        self.__y = value
 
     def area(self):
         """ Returns the area of the reactangle """
         return (self.__width * self.__height)
+
+    def display(self):
+        """
+        Prints in stdout the Rectangle instance with the character #
+        """
+        for y in range(0, self.__y):
+            print()
+        for i in range(0, self.__height):
+            for x in range(self.__x):
+                print(" ", end="")
+            for j in range(0, self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        name = type(self).__name__
+        i = self.id
+        x = self.x
+        y = self.y
+        w = self.width
+        h = self.height
+        return ("[{}] ({}) {}/{} - {}/{}".format(name, i, x, y, w, h))
+
+    def update(self, *args, **kwargs):
+        nb = len(args);
+        if nb > 0:
+            self.id = args[0]
+        if nb > 1:
+            self.width = args[1]
+        if nb > 2:
+            self.height = args[2]
+        if nb > 3:
+            self.x = args[3]
+        if nb > 4:
+            self.y = args[4]
+        if kwargs is not None:
+            if "id" in kwargs.keys():
+                self.id = kwargs.get('id')
+            if "width" in kwargs.keys():
+                self.width = kwargs.get('width')
+            if "height" in kwargs.keys():
+                self.height = kwargs.get('height')
+            if "x" in kwargs.keys():
+                self.x = kwargs.get('x')
+            if "y" in kwargs.keys():
+                self.y = kwargs.get('y')
