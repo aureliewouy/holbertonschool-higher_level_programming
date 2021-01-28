8538c9bacc17a9d160b70e08eff4a64ede48c53f
aureliewouy <aureliecedia@yahoo.com>
2021-01-28 16:34:48 +0000

    add more test for Square class

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -17,6 +17,26 @@ class TestSquare(unittest.TestCase):
         test = Square(2)
         self.assertEqual(test.size, 2)
 
+    def test_raise_type(self):
+        """Testing TypeError """
+        with self.assertRaises(TypeError):
+            Square("1")
+        with self.assertRaises(TypeError):
+            Square(1, "2")
+        with self.assertRaises(TypeError):
+            Square(1, 2, "3")
+
+    def test_raise_value(self):
+        """ Testing ValueError """
+        with self.assertRaises(ValueError):
+            Square(-1)
+        with self.assertRaises(ValueError):
+            Square(1, -2)
+        with self.assertRaises(ValueError):
+            Square(1, 2, -3)
+        with self.assertRaises(ValueError):
+            Square(0)
+
     def test_area(self):
         """
         Test the area function with the square class
@@ -34,6 +54,7 @@ class TestSquare(unittest.TestCase):
         s1.update(1, 2, 3, 4)
         self.assertEqual(str(s1),"[Square] (1) 3/4 - 2")
 
+
     def test_upkwargs(self):
         """
         Test the update with the kwargs
@@ -44,6 +65,18 @@ class TestSquare(unittest.TestCase):
         s1.update(size=7, id=89, y=1)
         self.assertEqual(str(s1),"[Square] (89) 12/1 - 7")
 
+    def test_save(self):
+        """ Testing save_to_file method """
+        test = Square.save_to_file([Square(1)])
+        self.assertEqual(test, None)
+        self.assertEqual(Square.save_to_file([]), None)
+
+    def test_create(self):
+        """ Testing create method """
+        test1 = Square(1, 9, 3)
+        test1_dict = test1.to_dictionary()
+        test2 = Square.create(**test1_dict)
+        self.assertEqual(type(test2), type(test2))
 
 if __name__ == '__main__':
     unittest.main()
