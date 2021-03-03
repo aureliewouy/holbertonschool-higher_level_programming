c31ac912c99be4c079bff0d51b11d9572098f594
aureliewouy <aureliecedia@yahoo.com>
2021-03-03 11:54:35 +0000

    add comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all privileges of the MySQL users
+-- Lists all privileges of the MySQL user 1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- User 2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
