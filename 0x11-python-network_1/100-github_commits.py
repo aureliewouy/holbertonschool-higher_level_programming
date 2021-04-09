c3d9471fd0af8d1b6cdfd28e70614711d7e566e1
aureliewouy <aureliecedia@yahoo.com>
2021-04-09 14:42:01 +0000

    update tasks

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Write a Python script that takes your GitHub credentials
+and uses the GitHub API to display your id"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """code should not be executed when imported"""
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    response = requests.get(url).json()
+    for r in response:
+        print("{}: {}".format(r.get('sha'),
+                              r.get('commit').get('author').get('name')))
