35ff191fa0c238ad080444f39342a3a948f99b67
aureliewouy <aureliecedia@yahoo.com>
2021-05-04 14:42:47 +0000

    id

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const url = process.argv.slice(2);
 const request = require('request');
-const wedge = '18';
 let nb = 0;
 request(url[0], function (error, response, body) {
   if (error) {
@@ -11,7 +10,7 @@ request(url[0], function (error, response, body) {
   const results = jsonBody.results;
   for (const element of results) {
     for (const c of element.characters) {
-      if (c === 'https://swapi-api.hbtn.io/api/people/' + wedge + '/') {
+      if (c.includes("18")) {
         nb = nb + 1;
       }
     }
